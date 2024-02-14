06485d415bd1f44cb2ea560a15595964d1f582f6
김준수 <asdg2795@naver.com>
2024-02-14 23:45:01 +0900

    18 - 오류 수정
    전체적인 오류로 인한 수정과정

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -3,12 +3,12 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
+
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "title"),
         @Index(columnList = "hashtag"),
@@ -20,22 +20,33 @@ public class Article extends AuditingFields {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount; // 유저 정보 (ID)
+
     @Setter @Column(nullable = false) private String title; // 제목
     @Setter @Column(nullable = false, length = 10000) private String content; // 본문
+
     @Setter private String hashtag; // 해시태그
+
     @ToString.Exclude
-    @OrderBy("id")
+    @OrderBy("createdAt DESC")
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+
     protected Article() {}
-    private Article(String title, String content, String hashtag) {
+
+    private Article(UserAccount userAccount, String title, String content, String hashtag) {
+        this.userAccount = userAccount;
         this.title = title;
         this.content = content;
         this.hashtag = hashtag;
     }
-    public static Article of(UserAccount entity, String title, String content, String hashtag) {
-        return new Article(title, content, hashtag);
+
+    public static Article of(UserAccount userAccount, String title, String content, String hashtag) {
+        return new Article(userAccount, title, content, hashtag);
     }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -45,9 +56,5 @@ public class Article extends AuditingFields {
     @Override
     public int hashCode() {
         return Objects.hash(id);
-    }
-
-    public UserAccount getUserAccount() {
-        return null;
     }
 }
--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -1,13 +1,9 @@
 package com.fastcampus.projectboard.domain;
-
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
-import java.util.LinkedHashSet;
 import java.util.Objects;
-import java.util.Set;
 
 @Getter
 @ToString(callSuper = true)
@@ -18,60 +14,36 @@ import java.util.Set;
 })
 @Entity
 public class ArticleComment extends AuditingFields {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Setter
-    @ManyToOne(optional = false)
-    private Article article; // 게시글 (ID)
-
-    @Setter
-    @JoinColumn(name = "userId")
-    @ManyToOne(optional = false)
-    private UserAccount userAccount; // 유저 정보 (ID)
-
-    @Setter
-    @Column(updatable = false)
-    private Long parentCommentId; // 부모 댓글 ID
-
-    @ToString.Exclude
-    @OrderBy("createdAt ASC")
-    @OneToMany(mappedBy = "parentCommentId", cascade = CascadeType.ALL)
-    private Set<ArticleComment> childComments = new LinkedHashSet<>();
+    @Setter @ManyToOne(optional = false) private Article article; // 게시글 (ID)
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount; // 유저 정보 (ID)
 
     @Setter @Column(nullable = false, length = 500) private String content; // 본문
 
 
     protected ArticleComment() {}
 
-    private ArticleComment(Article article, UserAccount userAccount, Long parentCommentId, String content) {
+    private ArticleComment(Article article, UserAccount userAccount, String content) {
         this.article = article;
         this.userAccount = userAccount;
-        this.parentCommentId = parentCommentId;
         this.content = content;
     }
 
     public static ArticleComment of(Article article, UserAccount userAccount, String content) {
-        return new ArticleComment(article, userAccount, null, content);
-    }
-
-    public void addChildComment(ArticleComment child) {
-        child.setParentCommentId(this.getId());
-        this.getChildComments().add(child);
+        return new ArticleComment(article, userAccount, content);
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof ArticleComment that)) return false;
-        return this.getId() != null && this.getId().equals(that.getId());
+        return id != null && id.equals(that.id);
     }
-
     @Override
     public int hashCode() {
-        return Objects.hash(this.getId());
+        return Objects.hash(id);
     }
-
 }
