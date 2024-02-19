c8d90a0e56d16245006136db72a62e3045e68227
asdg2795 <102370423+asdg2795@users.noreply.github.com>
2024-02-19 16:57:01 +0900

    Revert "#34 -  회원 계정 도메인 변경사항을 영향받는 코드에 적용"

--- src/main/java/com/fastcampus/projectboard/dto/UserAccountDto.java
+++ src/main/java/com/fastcampus/projectboard/dto/UserAccountDto.java
@@ -1,8 +1,11 @@
 package com.fastcampus.projectboard.dto;
+
 import com.fastcampus.projectboard.domain.UserAccount;
+
 import java.time.LocalDateTime;
 
 public record UserAccountDto(
+        Long id,
         String userId,
         String userPassword,
         String email,
@@ -13,12 +16,13 @@ public record UserAccountDto(
         LocalDateTime modifiedAt,
         String modifiedBy
 ) {
-    public static UserAccountDto of(String userId, String userPassword, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
-        return new UserAccountDto(userId, userPassword, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
+    public static UserAccountDto of(Long id, String userId, String userPassword, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new UserAccountDto(id, userId, userPassword, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
     }
 
     public static UserAccountDto from(UserAccount entity) {
         return new UserAccountDto(
+                entity.getId(),
                 entity.getUserId(),
                 entity.getUserPassword(),
                 entity.getEmail(),
@@ -30,6 +34,7 @@ public record UserAccountDto(
                 entity.getModifiedBy()
         );
     }
+
     public UserAccount toEntity() {
         return UserAccount.of(
                 userId,
@@ -39,4 +44,5 @@ public record UserAccountDto(
                 memo
         );
     }
+
 }
