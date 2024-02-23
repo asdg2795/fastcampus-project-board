5e415da0c8fda3a3efeb3fc8b6048ecb33d2fd38
김준수 <asdg2795@naver.com>
2024-02-23 15:12:28 +0900

    #45-introducing

--- src/main/java/com/fastcampus/projectboard/domain/constant/SearchType.java
+++ src/main/java/com/fastcampus/projectboard/domain/constant/SearchType.java
@@ -15,4 +15,7 @@ public enum SearchType {
         this.description = description;
     }
 
+
+    // 이렇게 함으로써 각 검색 유형에 대한 설명을 가지고 있는 열거형을 정의할 수 있습니다.
+    // 이를 통해 검색 옵션을 사용자에게 보여줄 때 각 옵션에 대한 설명을 함께 제공할 수 있습니다.
     }
